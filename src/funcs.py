02a11d5b119709be57fe3c1d060fd309e25e6469
markoc1120 <markoc1120@gmail.com>
2024-09-30 14:35:51 +0200

    answers

--- src/funcs.py
+++ src/funcs.py
@@ -6,9 +6,12 @@ def prod(a, b, c):
     Compute the product of three numbers.
 
     >>> prod(1, 2, 3)
-    'TEST ME!'
+    6
     """
-    ...
+    return a * b * c
+
+
+a = 6
 
 
 def prod2(b):
@@ -16,9 +19,10 @@ def prod2(b):
     Get a global a and write to a local c before computing prod(a, b, c)
 
     >>> prod2(42)
-    'TEST ME'
+    756
     """
-    ...
+    c = 3
+    return a * b * c
 
 
 def longest(x, y):
@@ -26,9 +30,9 @@ def longest(x, y):
     Returning the longest of two lists.
 
     >>> longest([1, 2, 3], [4, 5])
-    'TEST ME'
+    [1, 2, 3]
     """
-    ...
+    return x if len(x) > len(y) else y
 
 
 def dist(p1, p2):
@@ -36,8 +40,8 @@ def dist(p1, p2):
     Compute the distance between p1 and p2.
 
     >>> dist((1,2), (3,4))
-    'TEST ME'
+    2.83
     """
     x1, y1 = p1
     x2, y2 = p2
-    ...
+    return round(((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5, 2)
--- src/test_funcs.py
+++ src/test_funcs.py
@@ -2,25 +2,20 @@
 # all files that start with test_*.py and run all functions with
 # names that start with test_
 
-from funcs import (
-    prod,
-    prod2,
-    longest,
-    dist
-)
+from funcs import dist, longest, prod, prod2
 
 
 def test_prod():
-    assert prod(1, 2, 3) == 'TEST ME'
+    assert prod(1, 2, 3) == 6
 
 
 def test_prod2():
-    assert prod2(2) == 'TEST ME'
+    assert prod2(2) == 36
 
 
 def test_longest():
-    assert longest([1, 2, 3], [4, 5]) == 'TEST ME'
+    assert longest([1, 2, 3], [4, 5]) == [1, 2, 3]
 
 
 def test_dist():
-    assert dist((1, 2), (3, 4)) == 'TEST ME'
+    assert dist((1, 2), (3, 4)) == 2.83
